9706b2796a8fbbbf4ac12fb6d2772098dc4873f6
JaredArz <jared.arzate@gmail.com>
2023-10-12 20:04:54 -0500

    cleanup

--- mtj_time.py
+++ mtj_time.py
@@ -4,14 +4,9 @@ from interface_funcs import mtj_sample
 from mtj_types_v3 import SWrite_MTJ_rng
 import sys
 import math as m
-import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-colors = [
-          '#FFC20A',
-          '#0C7BDC',
-         ]
 cycles = 1000000
 reps = 10
 t_step = 5e-11
@@ -31,25 +26,6 @@ def main():
         times[rep] = (time.time() - start_time)
 
     print(f"--- %s seconds per {cycles} --- {np.average(times)}")
-
-def plot_init(fig):
-    plt.figure(fig)
-    plt.tight_layout()
-    plt.xlabel('ns')
-    plt.ylabel('mz')
-    plt.title(f'{mtj_type}')
-
-def plot(arr,fig,color):
-    plt.figure(fig)
-    scale = (t_step/1e-9)
-    x = np.arange(0,len(arr))
-    x = x * scale
-    plt.plot(x,arr,color=color)
-
-def save(fig):
-    plt.figure(fig)
-    plt.savefig(f"{mtj_type}_mz_cycs.png", dpi=1200, format='png')
-    plt.close()
 
 if __name__ == "__main__":
     main()
