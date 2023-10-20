38a6579b197fdd59c74eaa94406796f58d716c3a
kpatel68 <kpatel68@vols.utk.edu>
2023-10-20 17:56:45 -0400

    added distribution plots

---
+++ get_dist_plot.py
@@ -0,0 +1,33 @@
+import pickle
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.special import rel_entr
+from mtj_RL_dev import mtj_run
+
+SAMPLES = 1000
+
+
+if __name__ == "__main__":
+  
+  #TODO: Change param vals
+  alpha = 0.01
+  Ki = 0.0002
+  Ms = 300000.0
+  Rp = 500.0
+  TMR = 0.3
+  eta = 0.26508163213729863
+  J_she = 1000000000000.0
+  t_pulse = 3.790034800767898e-08
+  t_relax = 3.790034800767898e-08
+  d = 3e-09
+  tf = 1.1e-09
+  
+  _, _, _, countData, _, xxis, exp_pdf = mtj_run(alpha, Ki, Ms, Rp, TMR, d, tf, eta, J_she, t_pulse, t_relax, samples=SAMPLES)
+
+  plt.plot(xxis, countData, color="red", label="Actual PDF")
+  plt.plot(xxis, exp_pdf,'k--', label="Expected PDF")
+  plt.xlabel("Generated Number")
+  plt.ylabel("Normalized")
+  plt.title("PDF Comparison")
+  plt.legend()
+  plt.show()
