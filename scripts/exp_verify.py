eab0bf538a248a9a5bd3aecb64f5f067d2db3970
JaredArz <jared.arzate@gmail.com>
2024-01-01 15:12:26 -0600

    moving to lab comp

--- scripts/exp_verify.py
+++ scripts/exp_verify.py
@@ -44,6 +44,7 @@ RA_product = 3e-12
 V_to_J = lambda V:  V/RA_product
 # =================
 
+# V_50/t_50 in this code will mean the voltage/time to get a 0.5 probability of switching
 
 
 # ========== main functions =================
@@ -67,11 +68,10 @@ def gen_fig1_data():
 
     samples_to_avg = 8000
     Temps = [290, 300, 310]
-    pulse_duration = 1e-9
     for T in Temps:
-        generate_voltage_scurve(dev, voltages, pulse_duration, T, samples_to_avg, out_path=out_path, save_flag=True)
+        generate_voltage_scurve(dev, voltages, 1e-9, T, samples_to_avg, out_path=out_path, save_flag=True)
     np.savez(f"{out_path}/metadata_voltage.npz",
-             voltages=voltages, pulse_duration=pulse_duration, Temps=Temps)
+             voltages=voltages, pulse_duration=1e-9, Temps=Temps)
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
@@ -112,8 +112,8 @@ def make_and_plot_fig1():
     pf.prompt_save_svg(fig_t,f"../results/scurve_dataset_{date_match.group(0)}/pulse_duration_scurve.svg")
 
 def gen_fig2_data():
-    # Take voltage corresponding 0.5 probability for pulse duration of 1ns at 300K
-    # and compute the change in probability around that voltage for +-5k to get a measure of dp/dT
+    # Take voltage corresponding to 0.5 probability for pulse duration of 1ns at 300K
+    # and compute the change in probability around that voltage for +-5 [K] to get a measure of dp/dT
     #
     # Then for a range of pulse durations, repeat with a new 50% voltage for each
     #
@@ -121,30 +121,76 @@ def gen_fig2_data():
     # ====
 
     start_time = time.time()
-    samples_to_avg = 100 # will analyze deviation a bit differently in this script #TODO
+    samples_to_avg = 10000
     out_path = get_out_path()
-    pulse_durations = [8e-10, 1e-9, 2e-9]
-    voltages = np.linspace(-0.9,0,500)
+    pulse_durations = [1e-9, 2e-9, 3e-9, 4e-9]
+    voltages = np.linspace(-0.9 ,0 ,1000)
     dev = SWrite_MTJ_rng()
     dev.set_vals(0)
     dev.set_mag_vector()
 
-    num_iters = 5
-    T_delta = 5
-    Temps = (300-T_delta,300,300+T_delta) # two to calculate dT and 300K to calculate 0.5 probability voltage
+    T_delta = 15
+    Temps = (300-T_delta, 300+T_delta)
 
     V_50s = []
     for pulse_duration in pulse_durations:
-        for i in range(num_iters):
-            for T in Temps:
-                V_50 = generate_voltage_scurve(dev, voltages, pulse_duration, T, samples_to_avg,
-                                               i=i, out_path=out_path, save_flag=True)
-                if T == 300: V_50s.append(V_50)
+        for T in Temps:
+            generate_voltage_scurve(dev, voltages, pulse_duration, T, samples_to_avg,
+                                         out_path=out_path, save_flag=True)
+        V_50s.append(generate_voltage_scurve(dev, voltages, pulse_duration, 300, samples_to_avg,
+                                                  save_flag=False))
 
-    print(V_50s)
     np.savez(f"{out_path}/metadata_fig2.npz",
-             voltages=voltages, V_50s=V_50s, Temps=Temps, num_iters=num_iters, pulse_durations=pulse_durations)
+             voltages=voltages, V_50s=V_50s, Temps=Temps, pulse_durations=pulse_durations)
     print("--- %s seconds ---" % (time.time() - start_time))
+
+def make_and_plot_fig2():
+    fig, ax = pf.plot_init()
+    fig_v, ax_v = pf.plot_init()
+    dir_path = sys.argv[1]
+
+    metadata = np.load(glob.glob(dir_path + "/*metadata*")[0])
+    pulse_durations = metadata["pulse_durations"]
+    Temps = metadata["Temps"]
+    voltages = metadata["voltages"]
+    V_50s = metadata["V_50s"]
+    print(f"V_50s: {V_50s}")
+
+    colormap = plt.cm.get_cmap('viridis', len(pulse_durations)+1)
+    files = glob.glob(dir_path + "/*voltage_sweep*")
+
+    dT = Temps[1] - Temps[0]
+    print(f"dT: {dT}")
+    dpdT = []
+    #TODO: add measure of stddev
+    for i, pulse_duration in enumerate(pulse_durations):
+        # plot a pair of scurves for each pulse duration in addition
+        # to calculating dp/dT for good measure
+        weights_T0 = np.zeros(len(voltages))
+        weights_T1 = np.zeros(len(voltages))
+        V_50_idx = find_idx_at_nearest(voltages, V_50s[i])
+        f_data_T1 = np.load( match_file("voltage", files, pulse_duration, Temps[1], 0) )
+        f_data_T0 = np.load( match_file("voltage", files, pulse_duration, Temps[0], 0) )
+        weights_T1 = f_data_T1["weights"]
+        weights_T0 = f_data_T0["weights"]
+        dp = weights_T1[V_50_idx] - weights_T0[V_50_idx]
+        print(f"dp: {dp}")
+        dpdT.append(dp/dT)
+        ax_v.plot(voltages, weights_T0, color=colormap(i), alpha=0.7)
+        ax_v.plot(voltages, weights_T1, color=colormap(i), alpha=0.7)
+
+    ax.stem(pulse_durations, dpdT)
+    ax.axhline(0.0016)
+    ax.set_xscale('log')
+
+    ax.set_xlabel('Pulse Duration [s]')
+    ax.set_ylabel('dp/dT [K-1]')
+    ax.set_title('Coin Bias')
+
+    pf.prompt_show()
+    date_match = re.search(r'\d{2}:\d{2}:\d{2}', dir_path)
+    pf.prompt_save_svg(fig_v, f"../results/scurve_dataset_{date_match.group(0)}/fig2_curves.svg")
+    pf.prompt_save_svg(fig, f"../results/scurve_dataset_{date_match.group(0)}/fig2.svg")
 
 def gen_fig3_data():
     # Generate voltage scurve and directly compute a discrete dp/dV around p=0.5.
@@ -152,7 +198,7 @@ def gen_fig3_data():
     # ====
 
     start_time = time.time()
-    samples_to_avg = 100 # will analyze deviation a bit differently in this script #TODO
+    samples_to_avg = 5000
     out_path = get_out_path()
     pulse_durations = [1e-9, 1e-8, 1e-7]
     voltages = np.linspace(-0.9,0,250)
@@ -160,19 +206,65 @@ def gen_fig3_data():
     dev.set_vals(0)
     dev.set_mag_vector()
 
-    num_iters = 5
     T = 300
 
-    #FIXME
     V_50s = []
     for pulse_duration in pulse_durations:
-        for i in range(num_iters):
-            V_50s.append(generate_voltage_scurve(dev, voltages, pulse_duration, T, samples_to_avg,
-                                                 i=i, out_path=out_path, save_flag=True))
+        V_50s.append(generate_voltage_scurve(dev, voltages, pulse_duration, T, samples_to_avg,
+                                             out_path=out_path, save_flag=True))
 
     np.savez(f"{out_path}/metadata_fig3.npz",
-             voltages=voltages, V_50s=V_50s, num_iters=num_iters, pulse_durations=pulse_durations)
+             voltages=voltages, V_50s=V_50s, pulse_durations=pulse_durations)
     print("--- %s seconds ---" % (time.time() - start_time))
+
+def make_and_plot_fig3():
+    fig_a, ax_a = pf.plot_init()
+    fig_b, ax_b = pf.plot_init()
+    dir_path = sys.argv[1]
+
+    metadata = np.load(glob.glob(dir_path + "/*metadata*")[0])
+    num_iters = metadata["num_iters"]
+    pulse_durations = metadata["pulse_durations"]
+    voltages = metadata["voltages"]
+    V_50s = metadata["V_50s"]
+
+    colormap = plt.cm.get_cmap('viridis', len(pulse_durations)+1)
+    files = glob.glob(dir_path + "/*voltage_sweep*")
+
+    dpdV = []
+    #TODO: add measure of stddev
+    for i, pulse_duration in enumerate(pulse_durations):
+        dp_sum = 0.0
+        avg_weights = np.zeros(len(voltages))
+        for j in range(num_iters):
+            #FIXME
+            V_50_idx = find_idx_at_nearest(voltages, V_50s[i*num_iters+j])
+            f_data = np.load( match_file("voltage", files, pulse_duration, 300, j) )
+            weights = f_data["weights"]
+            dp_sum += (weights[V_50_idx + 1]) - (weights[V_50_idx - 1])
+            for k, weight in enumerate(f_data["weights"]): avg_weights[k] += weight
+        dV = (voltages[V_50_idx + 1]) - (voltages[V_50_idx - 1])
+        #FIXME: flipping sign to account for negative voltage
+        dpdV.append(-dp_sum/(dV*num_iters))
+        #Fig 3 a
+        ax_a.plot(voltages, [avg/num_iters for avg in avg_weights], color=colormap(i), alpha=0.7)
+
+    ax_a.set_title('Coin Bias')
+    ax_a.set_xlabel('Voltage [v]')
+    ax_a.set_ylabel('p')
+
+    ax_b.stem(pulse_durations, dpdV)
+    #TODO add analytical curve...
+    ax_b.set_xscale('log')
+
+    ax_b.set_xlabel('Pulse Duration [s]')
+    ax_b.set_ylabel('dp/dV [V-1]')
+    ax_b.set_title('Sensitivity to Voltage Amplitude')
+
+    pf.prompt_show()
+    date_match = re.search(r'\d{2}:\d{2}:\d{2}', dir_path)
+    pf.prompt_save_svg(fig_a, f"../results/scurve_dataset_{date_match.group(0)}/fig3a.svg")
+    pf.prompt_save_svg(fig_b, f"../results/scurve_dataset_{date_match.group(0)}/fig3b.svg")
 
 '''
 def gen_fig4_data():
@@ -207,111 +299,7 @@ def gen_fig4_data():
 
 
 
-def make_and_plot_fig2():
-    fig, ax = pf.plot_init()
-    fig_v, ax_v = pf.plot_init()
-    dir_path = sys.argv[1]
 
-    metadata = np.load(glob.glob(dir_path + "/*metadata*")[0])
-    num_iters = metadata["num_iters"]
-    pulse_durations = metadata["pulse_durations"]
-    Temps = metadata["Temps"]
-    voltages = metadata["voltages"]
-    V_50s = metadata["V_50s"]
-    print(V_50s)
-
-    colormap = plt.cm.get_cmap('viridis', len(pulse_durations)+1)
-    files = glob.glob(dir_path + "/*voltage_sweep*")
-
-    dT = Temps[-1] - Temps[0]
-    dpdT = []
-    #TODO: add measure of stddev
-    for i, pulse_duration in enumerate(pulse_durations):
-        dp_sum = 0.0
-        # plot a pair of scurves for each pulse duration in addition
-        # to calculating dp/dT for good measure
-        avg_weights_T0 = np.zeros(len(voltages))
-        avg_weights_T1 = np.zeros(len(voltages))
-        for j in range(num_iters):
-            V_50_idx = find_idx_of_nearest(voltages, V_50s[i*num_iters + j])
-            dp = 0.0
-            for sign, T in zip((-1,1), (Temps[0], Temps[-1])):
-                f_data = np.load( match_file("voltage", files, pulse_duration, T, j) )
-                p = (f_data["weights"])[V_50_idx]
-                dp += sign*p
-                # scurves, unecessary
-                if sign < 0:
-                    current = avg_weights_T0
-                else:
-                    current = avg_weights_T1
-                for l,w in enumerate(f_data["weights"]):
-                    current[l] += w
-            dp_sum += dp
-        dpdT.append(dp_sum/(dT*num_iters))
-        ax_v.plot(voltages, [avg/num_iters for avg in avg_weights_T0], color=colormap(i), alpha=0.7)
-        ax_v.plot(voltages, [avg/num_iters for avg in avg_weights_T1], color=colormap(i), alpha=0.7)
-
-    ax.stem(pulse_durations, dpdT)
-    ax.axhline(0.0016)
-    ax.set_xscale('log')
-
-    ax.set_xlabel('Pulse Duration [s]')
-    ax.set_ylabel('dp/dT [K-1]')
-    ax.set_title('Coin Bias')
-
-    pf.prompt_show()
-    date_match = re.search(r'\d{2}:\d{2}:\d{2}', dir_path)
-    pf.prompt_save_svg(fig_v, f"../results/scurve_dataset_{date_match.group(0)}/fig2_curves.svg")
-    pf.prompt_save_svg(fig, f"../results/scurve_dataset_{date_match.group(0)}/fig2.svg")
-
-def make_and_plot_fig3():
-    fig_a, ax_a = pf.plot_init()
-    fig_b, ax_b = pf.plot_init()
-    dir_path = sys.argv[1]
-
-    metadata = np.load(glob.glob(dir_path + "/*metadata*")[0])
-    num_iters = metadata["num_iters"]
-    pulse_durations = metadata["pulse_durations"]
-    voltages = metadata["voltages"]
-    V_50s = metadata["V_50s"]
-
-    colormap = plt.cm.get_cmap('viridis', len(pulse_durations)+1)
-    files = glob.glob(dir_path + "/*voltage_sweep*")
-
-    dpdV = []
-    #TODO: add measure of stddev
-    for i, pulse_duration in enumerate(pulse_durations):
-        dp_sum = 0.0
-        avg_weights = np.zeros(len(voltages))
-        for j in range(num_iters):
-            #FIXME
-            V_50_idx = find_idx_of_nearest(voltages, V_50s[i*num_iters+j])
-            f_data = np.load( match_file("voltage", files, pulse_duration, 300, j) )
-            weights = f_data["weights"]
-            dp_sum += (weights[V_50_idx + 1]) - (weights[V_50_idx - 1])
-            for k, weight in enumerate(f_data["weights"]): avg_weights[k] += weight
-        dV = (voltages[V_50_idx + 1]) - (voltages[V_50_idx - 1])
-        #FIXME: flipping sign to account for negative voltage
-        dpdV.append(-dp_sum/(dV*num_iters))
-        #Fig 3 a
-        ax_a.plot(voltages, [avg/num_iters for avg in avg_weights], color=colormap(i), alpha=0.7)
-
-    ax_a.set_title('Coin Bias')
-    ax_a.set_xlabel('Voltage [v]')
-    ax_a.set_ylabel('p')
-
-    ax_b.stem(pulse_durations, dpdV)
-    #TODO add analytical curve...
-    ax_b.set_xscale('log')
-
-    ax_b.set_xlabel('Pulse Duration [s]')
-    ax_b.set_ylabel('dp/dV [V-1]')
-    ax_b.set_title('Sensitivity to Voltage Amplitude')
-
-    pf.prompt_show()
-    date_match = re.search(r'\d{2}:\d{2}:\d{2}', dir_path)
-    pf.prompt_save_svg(fig_a, f"../results/scurve_dataset_{date_match.group(0)}/fig3a.svg")
-    pf.prompt_save_svg(fig_b, f"../results/scurve_dataset_{date_match.group(0)}/fig3b.svg")
 
 '''
 def make_and_plot_fig4():
@@ -337,7 +325,7 @@ def make_and_plot_fig4():
             #FIXME
             f_data = np.load( match_file("pulse", files, voltages[i], 300, j) )
             weights = f_data["weights"]
-            t_50_idx = find_idx_of_nearest(weights, 0.5) # parallel arrays
+            t_50_idx = find_idx_at_nearest(weights, 0.5) # parallel arrays
             dp_sum += (weights[t_50_idx + 1]) - (weights[t_50_idx - 1])
             for k, weight in enumerate(weights): avg_weights[k] += weight
         dt = (pulse_durations[t_50_idx + 1]) - (pulse_durations[t_50_idx - 1])
@@ -384,7 +372,7 @@ def avg_weight_across_samples(dev,V,T,samples_to_avg) -> float:
     sum_of_samples = np.sum([(mtj_sample(dev,V_to_J(V),T=T))[0] for _ in range(samples_to_avg)])
     return sum_of_samples/samples_to_avg
 
-def find_idx_of_nearest(arr, val) -> int:
+def find_idx_at_nearest(arr, val) -> int:
     return (np.abs(np.asarray(arr)-val)).argmin()
 
 def generate_pulse_duration_scurve(dev, durations, V, T, samples_to_avg, i=0, out_path=None, save_flag=True) -> float:
@@ -397,7 +385,7 @@ def generate_pulse_duration_scurve(dev, durations, V, T, samples_to_avg, i=0, ou
     for t in durations:
         dev.set_vals(t_pulse = t)
         weights.append(avg_weight_across_samples(dev, V, T, samples_to_avg))
-    t_50 = pulse_durations[find_idx_of_nearest(weights, 0.5)]
+    t_50 = pulse_durations[find_idx_at_nearest(weights, 0.5)]
     if save_flag:
         np.savez(f"{out_path}/pulse_duration_sweep_weights_v{V}_T{T}_i{i}.npz", weights=weights, T=T)
     return t_50
@@ -412,7 +400,7 @@ def generate_voltage_scurve(dev, voltages, t, T, samples_to_avg, i=0, out_path=N
         print(f"Generating voltage scurve with t: {t:.2e}s, T: {T}K, iteration: {i}")
     for V in voltages:
         weights.append(avg_weight_across_samples(dev, V, T, samples_to_avg))
-    V_50 = voltages[find_idx_of_nearest(weights, 0.5)]
+    V_50 = voltages[find_idx_at_nearest(weights, 0.5)]
     if save_flag:
         np.savez(f"{out_path}/voltage_sweep_scurve_data_t{t:.2e}_T{T}_i{i}.npz", weights=weights, T=T)
     return V_50
@@ -439,12 +427,12 @@ if __name__ == "__main__":
     print("===========================================================")
     if len(sys.argv) == 2:
 
-        make_and_plot_fig1()
-        #make_and_plot_fig2()
+        #make_and_plot_fig1()
+        make_and_plot_fig2()
         #make_and_plot_fig4()
         #make_and_plot_fig3()
     else:
         #gen_fig3_data()
         #gen_fig4_data()
-        #gen_fig2_data()
-        gen_fig1_data()
+        gen_fig2_data()
+        #gen_fig1_data()
