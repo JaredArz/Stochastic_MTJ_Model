a7813656cedba210712d4ca1be31780e3d83abbf
JaredArz <jared.arzate@gmail.com>
2024-02-09 15:06:34 -0600

    no

--- interface_funcs.py
+++ interface_funcs.py
@@ -33,22 +33,17 @@ def mtj_check(dev, V, cycles, pcs = None, rcs = None) -> (int,float):
     if p2pv > 0.25:
         nerror = -1
     else:
-        if dev.type == 0:
-            nerror = 0
-            if mz_c1 < 0.2:
-                mz_chk1_res = 0
-            elif mz_c1 < 0.5:
-                mz_chk1_res = 1
-            else: mz_chk1_res = -1
-        if dev.type == 0:
-            if mz_c2 < 0.2:
-                mz_chk2_res = -1
-            elif mz_c2 < 0.5:
-                mz_chk2_res = 1
-            else: mz_chk2_res = 0
-        if dev.type == 1:
-            print("error")
-            exit()
+        nerror = 0
+        if mz_c1 < 0.2:
+            mz_chk1_res = 0
+        elif mz_c1 < 0.5:
+            mz_chk1_res = 1
+        else: mz_chk1_res = -1
+        if mz_c2 < 0.2:
+            mz_chk2_res = -1
+        elif mz_c2 < 0.5:
+            mz_chk2_res = 1
+        else: mz_chk2_res = 0
 
         PI = 0
         if mz_chk1_res == -1:
