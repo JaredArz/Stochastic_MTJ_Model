e2c89c4bc8e7061aa4a452ab05d04adae6f7a60e
Alexander N Chin <anc200008@utdallas.edu>
2024-09-04 19:36:46 -0500

    Added SOT FL torque including xi DLFL ratio
    See Zhang and Joos

--- fortran_source/MTJ_RNG_vars.f90
+++ fortran_source/MTJ_RNG_vars.f90
@@ -14,7 +14,7 @@ module MTJ_RNG_vars
     real(kind(0.0d0)),parameter :: kb    = 1.380649e-23
     real(kind(0.0d0)),parameter :: eps_not = 8.8541878128e-12
     real(kind(0.0d0)),parameter :: gammall = 2.0*u0*uB/h_bar 
-    real(kind(0.0d0)),parameter :: gammab  = gammall/u0
+    real(kind(0.0d0)),parameter :: gammab  = gammall/u0                     !
     real(kind(0.0d0)),parameter :: t_step  = 5e-11
     real(kind(0.0d0)),parameter :: eps_mgo = 4.0
     real(kind(0.0d0)),parameter :: ksi = 75e-15
@@ -22,6 +22,7 @@ module MTJ_RNG_vars
     real(kind(0.0d0)),parameter :: w   = 100e-9
     real(kind(0.0d0)),parameter :: l   = 100e-9
     real(kind(0.0d0)),parameter :: rho = 200e-8
+    real(kind(0.0d0)),parameter :: alphar = 0
     real(kind(0.0d0)),parameter :: P   = 0.6
     real(kind(0.0d0)) :: Hx = 0.0
     real(kind(0.0d0)) :: Hy = 0.0
@@ -63,7 +64,7 @@ module MTJ_RNG_vars
 
             A1      = a*b*pi/4.0_dp
             Bsat    = Ms*u0
-            gammap  = gammall/(1.0_dp+alpha*alpha)
+            gammap  = gammall/(1.0_dp+alpha*alpha)                                      !gyromagnetic ratio
             volume  = tf*pi*b*a/4.0_dp
             cap_mgo = eps_not*eps_mgo*A1/tox 
             Htherm  = sqrt((2.0_dp*u0*alpha*kb*T)/(Bsat*gammab*t_step*volume))/u0
